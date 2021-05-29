3a4ea3d1f87c4edcce30f07f87dd08a30f8ada13
LM7 <legitmichel777@outlook.com>
2021-05-29 22:33:52 +0800

    Initial Commit

---
+++ Hangman/HangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanApp.swift
+//  Hangman
+//
+//  Created by LegitMichel777 on 2021/5/29.
+//
+
+import SwiftUI
+
+@main
+struct HangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
