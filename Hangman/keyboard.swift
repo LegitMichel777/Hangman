698c518403f0dc31f89c89f78885322ce35b011c
LM7 <legitmichel777@outlook.com>
2021-05-30 09:52:03 +0800

    Added keyboard

--- Hangman/keyboard.swift
+++ Hangman/keyboard.swift
@@ -7,14 +7,76 @@
 
 import SwiftUI
 
-struct keyboard: View {
+let keyboardLayout=[["Q","W","E","R","T","Y","U","I","O","P"],["A","S","D","F","G","H","J","K","L"],["Z","X","C","V","B","N","M"]]
+
+struct MaximumWidthPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = max(value, nextValue())
+    }
+}
+
+struct DetermineWidth: View {
+    typealias Key = MaximumWidthPreferenceKey
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader {
+            proxy in
+            Color.clear
+                .anchorPreference(key: Key.self, value: .bounds) {
+                    anchor in proxy[anchor].size.width
+                }
+        }
+    }
+}
+
+struct indivKey: View {
+    var key: String
+    var body: some View {
+        ZStack(alignment: .top) {
+            RoundedRectangle(cornerRadius: 5)
+                .frame(height:46)
+                .foregroundColor(.init("keyShadow"))
+            ZStack {
+                RoundedRectangle(cornerRadius: 5)
+                    .frame(height:44)
+                    .foregroundColor(.init("key"))
+                Text(key)
+                    .font(.system(size: 18, weight: .regular, design: .default))
+            }
+        }
+        
+    }
+}
+
+struct keyboard: View {
+    @State var maxWidth: CGFloat = 0
+    var engine: hmEngine
+    var body: some View {
+        let betweenKeyDiff: CGFloat = 6
+        let betweenRowDiff: CGFloat = 7
+        VStack(spacing: betweenRowDiff) {
+            HStack(spacing:betweenKeyDiff) {
+                ForEach((0..<keyboardLayout[0].count)) { index2 in
+                    indivKey(key: keyboardLayout[0][index2])
+                        .overlay(DetermineWidth())
+                }
+            }
+            ForEach((1..<keyboardLayout.count), id: \.self) { index1 in
+                HStack(spacing: betweenKeyDiff) {
+                    ForEach((0..<keyboardLayout[index1].count)) { index2 in
+                        indivKey(key: keyboardLayout[index1][index2])
+                            .frame(width:maxWidth)
+                    }
+                }
+            }
+        }.onPreferenceChange(DetermineWidth.Key.self) {
+            maxWidth=$0
+        }.padding(.horizontal,7)
     }
 }
 
 struct keyboard_Previews: PreviewProvider {
     static var previews: some View {
-        keyboard()
+        keyboard(engine: hmEngine())
     }
 }
